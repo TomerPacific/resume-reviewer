eef93b0803348fa5287bbc07a48bda59c2545fec
unknown <tomerpacific@gmail.com>
2019-11-25 17:50:55 +0200

    resume-reviewer adding footer element to project structure

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,39 @@
     <main>
       <router-view/>
     </main>
+    <Footer></Footer>
   </div>
 
 </template>
 
 <script>
  import AppNav from './components/AppNav.vue'
+ import Footer from './components/Footer.vue'
 
  export default {
      name: 'app',
      components:{
-        AppNav
+        AppNav,
+        Footer
      }
  }
 </script>
   
-<style>
+<style scoped>
+
+html, body {
+  height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100%
 }
 
 </style>
