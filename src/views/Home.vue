033d4ff747ec86946c129d35d956d1a518baa3fc
unknown <tomerpacific@gmail.com>
2019-11-27 22:04:45 +0200

    resume-reviewer adding content to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,23 @@
   <div class="home">
     <img id="interview" src="../assets/interview.jpg" alt="interview" title="Photo by Rachel Danner on Unsplash">
     <p>Welcome To Resume Reviewer! <br>
-      Your one-stop-shop for all everything that you need to prepare yourself for a job interview</p>
+      Your one-stop-shop for everything that you need to prepare yourself for a job interview<br>
+    </p>
+    <br>
+    <p>We do the following:</p>
+
+    <div>
+      <ul id="points">
+        <li>Reviewing Your Resume</li>
+        <li>Pre Interview Training</li>
+        <li>Bheavioral Questions</li>
+        <li>Culture Fit Preperation</li>
+        <li>Whiteboard Practice</li>
+        <li>Pair Programming Exercises</li>
+      </ul>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -31,5 +46,18 @@ export default {
     height: 30%;
     width: 30%;
     float: left;
+  }
+
+  #points {
+    list-style: none;
+    margin-top: 3%;
+  }
+
+  #points li {
+    margin-bottom: 1%;
+  }
+
+  #points li:hover {
+    color: #0088A9;
   }
 </style>
