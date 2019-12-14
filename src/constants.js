e889489d1a301b914cd716de97c62b221b41f204
unknown <tomerpacific@gmail.com>
2019-12-14 19:54:53 +0200

    resume-reviewer creating constants for errors

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,8 @@ export default {
     HEBREW_LANGUAGE: 'Hebrew',
     ENGLISH_LANGUAGE: 'English',
     TEXT_DIRECTION_RTL: 'rtl',
+    errors: {
+        USER_NOT_LOGGED_IN: 'User Not Logged In',
+        INVALID_FILE_UPLOAD: 'Invalid File'
+    }
 };
