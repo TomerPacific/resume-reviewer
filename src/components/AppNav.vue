c3076761e20495b1a2075a95dff086e380406b0b
unknown <tomerpacific@gmail.com>
2019-12-20 16:21:13 +0200

    resume-reviewer changing styles and layout to reflect vue bootstrap usage

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,28 +1,23 @@
 <template>
-<header dir="ltr">
-    <img src="../assets/logo.png" id="logo">
-    <ul id="navigation" v-if="language === ENGLISH_LANGUAGE">
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/file">Upload Resume</router-link></li>
-    </ul>
-    <ul id="navigation" v-if="language === HEBREW_LANGUAGE">
-        <li><router-link to="/">דף בית</router-link></li>
-        <li><router-link to="/about">אודות</router-link></li>
-        <li><router-link to="/file">העלאת קורות חיים</router-link></li>
-    </ul>
-    <div id="login" v-if="!isUserLoggedIn">
-      <form @submit="checkForm" action="" method="">
-        <i class="fas fa-user"></i><input id="username" type="email" value="" :placeholder="language === HEBREW_LANGUAGE ? 'אימייל' : 'Email'" pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" v-model="username"> 
-        <input type="submit" :value="language === HEBREW_LANGUAGE ? 'התחברות' : 'Log In'" id="login_btn" @click="login"> <br>
-        <i class="fas fa-key"></i><input id="password" type="password" value="" :placeholder="language === HEBREW_LANGUAGE ? 'סיסמא' : 'Password'" v-model="password">
-        <input type="submit" :value="language === HEBREW_LANGUAGE ? 'הרשמה' : 'Sign Up'" id="signin_btn" @click="signin">
-      </form>
-    </div>
-    <div id="logut" v-else>
-      <button id="logout_btn" v-on:click="logoutUser">{{language === HEBREW_LANGUAGE ? 'התנתק' : 'Logout' }}</button>
-    </div>
-</header>
+  <header dir="ltr">
+    <b-navbar toggleable="lg" fill>
+    <b-navbar-brand>
+      <img src="../assets/logo.png" id="logo">
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+        <b-nav-item to="/file">Upload Resume</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </header>
 </template>
 
 
@@ -117,82 +112,22 @@ export default {
     background-color: #24252A;
   }
 
-  li,a {
-    font-weight: 500;
-    font-size: 20px;
-    color: #EDF0F1;
-    text-decoration: none;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 30px 5%;
-    border: 2px solid white;
-  }
-
-  #navigation {
-    list-style-type: none;
-    font-family: 'Montserrat', sans-serif;
-  }
-
-  #navigation li {
-    display: inline-block;
-    padding: 0px 20px;
-  }
-
-  #navigation li a {
-    transition: all 0.3s ease 0s;
-  }
-
-  #navigation li a:hover {
-    color: #0088A9;
-  }
-
   #logo {
     width: 50px;
     height: 50px;
   }
 
-  #login {
-    color: white;
+  .nav-link {
+    color:white !important;
   }
 
-  #login form input {
-    display: inline-block;
-    color: white;
-    margin-bottom: 2%;
+  .nav-link:hover {
+     color: #0088A9 !important;
   }
+.nav-link {
+  transition: all 0.3s ease 0s;
+}
 
-  ::-webkit-input-placeholder, :-ms-input-placeholder, ::-moz-placeholder, ::placeholder {
-    color: white;
-  }
 
-  #login_btn {
-    position: absolute;
-    right: 5;
-    width: 55px;
-  }
-
-  #signin_btn {
-      position: absolute;
-      right: 5;
-      width: 55px;
-  }
-
-  #logout_btn {
-    color: white;
-    font-weight: 800;
-    width: 55px;
-  }
-
-  #signin_btn:hover, #login_btn:hover, #logout_btn:hover {
-     color: #0088A9;
-  }
-
-  .missing_input {
-    border: 2px solid red;
-  }
 
 </style>
