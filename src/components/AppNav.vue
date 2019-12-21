63bd31744af560af331a61f93dd5a6eb0d3d3888
unknown <tomerpacific@gmail.com>
2019-12-21 15:42:48 +0200

    resume-reviewer login/signup form to navbar in vue bootstrap

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -14,12 +14,18 @@
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+        <b-nav-form @submit="checkForm" action="" method="">
+          <i class="fas fa-user"></i><b-form-input class="mr-sm-2" :placeholder="language === HEBREW_LANGUAGE ? 'אימייל' : 'Email'" pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" v-model="username"></b-form-input>
+          <b-form-input class="mr-sm-2" :placeholder="language === HEBREW_LANGUAGE ? 'סיסמא' : 'Password'" v-model="password"></b-form-input>
+          <b-button variant="outline-primary" class="my-2 my-sm-0" type="submit" :value="language === HEBREW_LANGUAGE ? 'הרשמה' : 'Sign Up'" id="signin_btn" @click="signin">{{ language === HEBREW_LANGUAGE ? 'הרשמה' : 'Sign Up' }}</b-button>
+          <b-button variant="outline-success" class="my-2 my-sm-0" type="submit" :value="language === HEBREW_LANGUAGE ? 'התחברות' : 'Log In'" id="login_btn" @click="login">{{ language === HEBREW_LANGUAGE ? 'התחברות' : 'Log In' }} </b-button>
+
+        </b-nav-form>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
   </header>
 </template>
-
 
 <script>
 
